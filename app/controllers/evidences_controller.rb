d8d790202b68e6ea193f1c9e047ee272fd1b3636
Monma_Takeshi <jemini0527@gmail.com>
2020-09-23 01:35:27 +0900

    ビューに渡す仮置インスタンス作成 evidences_controller.rb
    evidences#newアクションに渡す仮置のインスタンスを作成(DBに紐付いていないとsimple_formが使えないため)。後ほど変更予定。

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -1,7 +1,10 @@
 class EvidencesController < ApplicationController
   def new
+    @evidences = Evidence.new
   end
 
   def create
+    # 成功した場合は直接showアクションへ遷移予定
+    redirect_to root_path
   end
 end
