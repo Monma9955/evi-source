5ec513168f4f637cacd1f4cc8a715ef88ba724e3
Monma_Takeshi <jemini0527@gmail.com>
2020-10-07 00:59:29 +0900

    バリデーション記載 post.rb
    post.rbにバリデーション記載。
    投稿の入力を必須にしました。

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   has_many :evidences, dependent: :destroy
 
   accepts_nested_attributes_for :evidences, allow_destroy: true
+
+  validates :text, presence: true
 end
