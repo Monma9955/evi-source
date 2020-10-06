4b5508dc2de2c12b4b57ff8afa3ad520ada11c47
Monma_Takeshi <jemini0527@gmail.com>
2020-10-07 01:24:21 +0900

    単体テスト post_spec.rb
    postsテーブルの単体テストを実施。
    カラムはtextのみのためtextカラムが登録できる場合とできない場合のみテストしました。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post do
+  describe '#create' do
+    context '登録可能' do
+      it "textが存在すれば登録できること" do
+        expect(build(:post)).to be_valid
+      end
+    end
+
+    context '登録不可' do
+      it "textが存在しない場合は登録できないこと" do
+        post = build(:post, text: nil)
+        post.valid?
+        expect(post.errors[:text]).to include(I18n.t('errors.messages.blank'))
+      end
+    end
+  end
 end
