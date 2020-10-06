a0c0cb49cd70f0a7806cba72c6d8f12cfad2fc76
Monma_Takeshi <jemini0527@gmail.com>
2020-10-07 01:22:20 +0900

    factoriesファイル作成 ecidences.rb
    evidencesテーブルのfactoriesファイルを作成しました。

--- spec/factories/evidences.rb
+++ spec/factories/evidences.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
+
   factory :evidence do
-    
+    source                { "https://www.e-stat.go.jp/" }
+    informant             { "e-Stat(総務省統計局)" }
+    source_updated_on     { "20200527" }
+    level                 { 0 }
+    fact_or_opinion       { 0 }
+    data_type             { 0 }
+    reference             { "東京都の人口総数を参照" }
+    association :post
   end
+
 end
