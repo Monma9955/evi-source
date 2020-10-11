2d09c9d9bad7c57b1638d935f3216d72416613fe
Monma_Takeshi <jemini0527@gmail.com>
2020-10-12 00:43:58 +0900

    evidences enumカラムのfactoryデータ変更
    FactoryBotのenumカラムのデータが数字だとArgumentErrorが発生するため、フォームで選択するときと同じ文字列にしました。

--- spec/factories/evidences.rb
+++ spec/factories/evidences.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
     source                { "https://www.e-stat.go.jp/" }
     informant             { "e-Stat(総務省統計局)" }
     source_updated_on     { "20200527" }
-    level                 { 0 }
-    fact_or_opinion       { 0 }
-    data_type             { 0 }
+    level                 { "primary" }
+    fact_or_opinion       { "fact" }
+    data_type             { "statistical" }
     reference             { "東京都の人口総数を参照" }
     association :post
   end
