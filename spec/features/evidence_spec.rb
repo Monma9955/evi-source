7c1676db4438b2dc168d8a2026a09e5ca60e6046
Monma_Takeshi <jemini0527@gmail.com>
2020-10-12 19:33:12 +0900

    フィーチャスペックテスト 投稿機能のテスト
    フィーチャスペックでPostとEvidenceを投稿する動作のテストを実施。

---
+++ spec/features/evidence_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'evidences', type: :feature do
+
+  scenario 'ユーザーはPostとEvidenceを投稿する' do
+    visit root_path
+
+    # ルートパスがpost・evidenceの投稿画面(evidences#new)である
+    expect(page).to have_content "投稿内容"
+    expect(page).to have_content "根拠・情報源"
+    expect(page).to have_content "情報提供元"
+    expect(page).to have_content "情報の最終更新日時"
+    expect(page).to have_content "情報レベル"
+    expect(page).to have_content "情報の種類"
+    expect(page).to have_content "備考"
+    expect(page).to have_button "登録する"
+
+    # post・evidenceの投稿で各テーブルにレコードが増えている
+    expect {
+      fill_in '投稿内容', with: "ああ"
+      fill_in '根拠・情報源', with: "ああ"
+      fill_in '情報提供元', with: "ああ"
+      select "2000", from: 'post_evidences_attributes_0_source_updated_on_1i'
+      select "1月", from: 'post_evidences_attributes_0_source_updated_on_2i'
+      select "1", from: 'post_evidences_attributes_0_source_updated_on_3i'
+      select "一次情報", from: '情報レベル'
+      select "事実", from: '事実か私見か'
+      select "統計", from: '情報の種類'
+      fill_in "備考", with: "ああ"
+      click_button '登録する'
+    }.to change(Post, :count).by(1) & change(Evidence, :count).by(1)
+  end
+
+end
