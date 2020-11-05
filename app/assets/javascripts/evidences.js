00d7126382e157d74974866548a40baf88b5c8e8
Monma_Takeshi <jemini0527@gmail.com>
2020-11-05 20:56:32 +0900

    投稿前の警告を追加
    フォーム投稿前に誤ってEnterキーを２回押下すると入力中にフォームの内容が投稿されてしまうため、投稿がキャンセルできるようjqueryで確認メッセージを追加しました。

--- app/assets/javascripts/evidences.js
+++ app/assets/javascripts/evidences.js
@@ -1,9 +1,16 @@
-
 $(document).on('turbolinks:load', function() {
   // 最初のevidenceフォームは削除できないよう、削除ボタンを非表示化
   if ($('.nested-fields').length == 1) {
     $('#remove-btn').hide();
   }
+
+  // 投稿ボタンをクリックするとイベント発火
+  $('#postBtn').click(function() {
+    if(!confirm("入力を終了し、投稿してよろしいですか？")) {
+      // キャンセルを押したら投稿をキャンセル
+      return false;
+    }
+  });
 
   $(function(){
     // evidences一覧のアコーディオンメニュー表示・非表示イベント
