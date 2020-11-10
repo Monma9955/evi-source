54c7e7ae686aff99cbbb57b5a9a06105d99babbc
Monma_Takeshi <jemini0527@gmail.com>
2020-11-10 22:20:02 +0900

    リファクタリング 短縮URL作成用のメソッド作成
    evidences_controllerをリファクタリング。
    短縮URLを作成する機能short_urlメソッドに切り出しました。

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -15,7 +15,8 @@ class EvidencesController < ApplicationController
           client = Bitly::API::Client.new(token: ENV['BITLY_TOKEN'])
           shorturl = client.shorten(long_url: evidence.source)
           @short_url = ShortUrl.new({evidence_id: evidence.id,
-                                      url: shorturl.link})
+                                      url: short_url(evidence.source)})
+          # 保存ができなかったパターンの処理は未記載
           @short_url.save
         end
       end
@@ -26,6 +27,14 @@ class EvidencesController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+
+  def short_url(long_url)
+    # bitlyのAPIを使用して短縮URLを作成
+    client = Bitly::API::Client.new(token: ENV['BITLY_TOKEN'])
+    shorturl = client.shorten(long_url: long_url)
+    shorturl.link
   end
 
   private
