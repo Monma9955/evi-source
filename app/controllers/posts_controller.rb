d3db80eba08ff0b6da584057103b16c7fb0a1bfd
Monma_Takeshi <jemini0527@gmail.com>
2021-01-26 23:22:59 +0900

    投稿一覧昇順・降順並べ替え機能
    投稿一覧の昇順・降順をボタンで並べ替えられるようにしました。seedデータを使う関係上idで昇順・降順を並べ替えるロジックにしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    #seedで入れたデータはcreated_atが同じ時間のためidで並べ替え
+    #ログイン機能未実装のためPostデータはallで取得
+    if params[:sort] == nil || params[:sort] == "desc"
+      @posts = Post.all.order('id DESC')
+      @sort = "asc"
+    elsif params[:sort] == "asc"
+      @posts = Post.all.order('id ASC')
+      @sort = "desc"
+    end
   end
 end
