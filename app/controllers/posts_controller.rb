90869fdc23e3c8dfa73d626c21f14d020090cc18
Monma_Takeshi <jemini0527@gmail.com>
2021-01-27 23:02:49 +0900

    sider指摘箇所修正
    #のあとにスペースを入れるよう警告があったため挿入。また、コメントの内容がわかりやすいよう変更しました。また、hamlではハッシュリテラル内にスペースを追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    #seedで入れたデータはcreated_atが同じ時間のためidで並べ替え
-    #ログイン機能未実装のためPostデータはallで取得
+    # 本来created_atで並べ替えたいが、データをseedで入れると同時になってしまうためidで並べ替え
+    # ログイン機能未実装のためPostデータはallで取得
     if params[:sort] == nil || params[:sort] == "desc"
       @posts = Post.all.order('id DESC')
       @sort = "asc"
