9328b2a1ffb72872874cdd34ec7c0c6c47e2800c
Monma_Takeshi <jemini0527@gmail.com>
2021-01-29 23:50:38 +0900

    Posts#indexリクエストのテスト
    request_specでPostコントローラーのindexアクションのリクエストをテストしました。

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/posts/index"
-      expect(response).to have_http_status(:success)
+    let!(:posts) { create(:post) }
+
+    before do
+      get posts_url
+    end
+
+    it "リクエストが成功すること" do
+      expect(response.status).to eq 200
+    end
+
+    it "投稿が表示されていること" do
+      expect(response.body).to include "平成27年の東京都の人口は13,515,271人である。"
     end
   end
 end
