e731809b6426352539ec30930a48b51cc2259a3a
Monma_Takeshi <jemini0527@gmail.com>
2021-02-01 04:24:07 +0900

    factory拡張 posts.rb
    posts.rbのfactoryを拡張。投稿が複数ある場合のテストができるようにしました。

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :post do
     text  { "平成27年の東京都の人口は13,515,271人である。" }
+
+    # 投稿を複数用意するシチュエーションで使用
+    trait :posts_a do
+      text { "1番目の投稿" }
+    end
+
+    trait :posts_b do
+      text { "2番目の投稿" }
+    end
+
+    trait :posts_c do
+      text { "3番目の投稿" }
+    end
   end
 end
