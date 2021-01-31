8190549e2374a5cc98b4568bc52eb56f6a88930c
Monma_Takeshi <jemini0527@gmail.com>
2021-02-01 04:26:22 +0900

    feature specテスト post_spec.rb 複数投稿をテスト
    post_spec.rbで複数投稿されている場合の投稿順序変更が正常に行われているかテストしました。

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 feature 'posts', type: :feature do
-  given!(:posts) { create(:post) }
+  given!(:posts_a) { create(:post, :posts_a) }
+  given!(:posts_b) { create(:post, :posts_b) }
+  given!(:posts_c) { create(:post, :posts_c) }
 
   scenario 'ユーザーは一覧ページからPostの並び順を変更する' do
     visit posts_path
@@ -9,11 +11,28 @@ feature 'posts', type: :feature do
     # Post一覧ページが表示され、投稿も表示されている
     expect(page).to have_content "投稿されたエビデンス一覧"
     within '.number1' do
-      expect(page).to have_content "平成27年の東京都の人口は13,515,271人である。"
+      expect(page).to have_content "3番目の投稿"
     end
+    within '.number2' do
+      expect(page).to have_content "2番目の投稿"
+    end
+    within '.number3' do
+      expect(page).to have_content "1番目の投稿"
+    end
+
     # 並び順変更ボタンをクリックする
     click_button '新着順'
-    # ボタンの名称が変わっている
+
+    # ボタンの名称が変わり、投稿の順番が変わっている
     expect(page).to have_content "古い順"
+    within '.number1' do
+      expect(page).to have_content "1番目の投稿"
+    end
+    within '.number2' do
+      expect(page).to have_content "2番目の投稿"
+    end
+    within '.number3' do
+      expect(page).to have_content "3番目の投稿"
+    end
   end
 end
